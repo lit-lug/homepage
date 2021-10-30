cb9fab5db57b84cd9fa905c742fe690b18ab31eb
icpove <gaoajia@qq.com>
2021-10-30 14:37:01 +0800

    baiduPush

--- baiduPush.sh
+++ baiduPush.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-#set -e
+set -e
 
 # 百度链接推送
-#curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://iluoli.ren&token="
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://litunix.org&token=3i1DMydbbUt7u5UZ"
 
-#rm -rf urls.txt # 删除文件
+rm -rf urls.txt # 删除文件
