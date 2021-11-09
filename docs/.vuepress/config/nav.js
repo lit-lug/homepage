51af340eeed594b59d18064dff0f0d71eb669035
icpove <gaoajia@qq.com>
2021-11-09 19:56:24 +0800

    update

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,7 +1,7 @@
 module.exports = [
   { text: 'Home', link: '/' },
   { text: 'Blog', link: "/"},
-  { text: 'wiki', link: "/"},
+  { text: 'wiki', link: "/pages/c7906c/"},
   { text: 'Mirrors', link: "/"},
   { text: 'About', link: "/pages/about/"},
 ]
